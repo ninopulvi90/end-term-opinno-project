58ceb6a153ba14282390450dbf6a8d6d41aaaa45
Your Name <RobertasDR>
2023-12-14 17:35:14 +0100

    Footer e struttura home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import './Home.css';
+import Footer from '../components/Footer';
+
+export default function(){
+    return (
+            <main>
+            <div className="menu_laterale">
+                <div>Profile</div>
+                <div>Sidebar</div>
+                <Footer></Footer> 
+            </div>
+            <div className='centrale'>
+                <nav>Navbar</nav>
+                <div className="wrapper">                               {/* wrapper */}
+                    <section>Add Post</section>
+                    <section>Post</section>
+                </div>
+
+            </div>   
+        </main>
+    )
+}
