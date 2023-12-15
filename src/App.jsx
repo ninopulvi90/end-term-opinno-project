c485c80e05abe22051fabe19ddbc9a76fbb3546b
Your Name <RobertasDR>
2023-12-15 16:10:47 +0100

    Pagina Errore

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Error from "./pages/Error";
+
 
 export default function App() {
   return <div className="flex justify-center">
       <Router>
         <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/error" element={<Error />} />
         </Routes>
       </Router>
   </div>
---
+++ src/pages/Error.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function Error(){
+    return (
+        <div className="container flex flex-col justify-center items-center w-screen h-screen ">
+            <img className='w-[800px]' src="src\assets\war-zone-landscape-with-apocalyptic-destruction_23-2150985733.jpg" alt="Non abbiamo trovato nemmeno l'immagine" />
+            <h1 className='text-6xl font-extrabold mt-10'>ERRORE 404</h1>
+            <h1 className='text-4xl font-extrabold mt-4'>Stacca! Stacca!</h1>
+            <button className="rounded-2xl mt-10 bg-white text-black hover:bg-[#006793] hover:text-white border-solid border-2 border-black text-white font-bold py-2 px-4 rounded"><Link to="/">Home</Link></button>
+            {/* <h2 className='text-2xl mt-10'>Non sei nel posto giusto, ma almeno hai visto quanto siamo belli</h2> */}
+        </div>
+    )
+}
