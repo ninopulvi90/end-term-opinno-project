3bcfb3c2dea1c42bd61732181c71c224ba6a1384
Nino Pulvirenti <ninoresin@gmail.com>
2023-12-15 16:57:54 +0100

    added container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import PostContainer from "../components/posts-container/PostContainer";
 
 export default function Home () {
   return (
-    <main>
+    <main className="container">
       <div className="menu_laterale">
         <Sidebar />
 
