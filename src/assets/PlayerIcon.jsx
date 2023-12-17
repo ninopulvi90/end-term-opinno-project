dd017b8de33b1dcc109576b39cc36377c0f85100
claudiastem <claudia.italiani@hotmail.it>
2023-12-17 12:29:16 +0100

    modified PlayerIcon image and added Profile picture and name

--- src/assets/PlayerIcon.jsx
+++ src/assets/PlayerIcon.jsx
@@ -6,16 +6,32 @@ const PlayerIcon =
         strokeWidth={1.5}
         stroke="currentColor"
         className="w-5 h-5 mx-2 dark:stroke-[#FFFFFF]">
+
         <path
             strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-        />
-        <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
+            d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
         />
     </svg>
 
 export default PlayerIcon
+
+{/* <svg
+xmlns="http://www.w3.org/2000/svg"
+fill="none"
+viewBox="0 0 24 24"
+strokeWidth={1.5}
+stroke="currentColor"
+className="w-5 h-5 mx-2 dark:stroke-[#FFFFFF]">
+
+<path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+/>
+
+<path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
+/>
+</svg> */}
