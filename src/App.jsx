353f886dc552a8ae96f20851841f07385318277a
Your Name <RobertasDR>
2023-12-18 10:10:28 +0100

    Modifiche background

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import RightSidebar from "./components/right-sidebar/RightSidebar";
 
 
 export default function App() {
-  return <div className="flex justify-center dark:bg-smp-black bg-smp-white bg-[url(https://picsum.photos/2000/1500)]">
+  return <div className="flex justify-center dark:bg-[url(src/assets/sfondoScuro.png)] bg-smp-white bg-[url(src/assets/sfndoAz.png)] bg-cover bg-no-repeat bg-fixed">
       <Router>
         <Routes>
         <Route path="/" element={<Home />} />
